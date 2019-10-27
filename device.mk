178512b6fe7c8c6550b7985990b0291da3cfdcd0
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-27 19:21:36 +0100

    miatoll: Switch to AOSP NFC
    Change-Id: I9f62b369181c75e8ad5e0b8504f6c2fbb292f894

--- device.mk
+++ device.mk
@@ -156,24 +156,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/nxp/opensource/pn5xx
-
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    com.gsma.services.nfc \
-    com.nxp.nfc.nq \
-    libnqnfc_nci_jni \
-    NQNfcNci \
+    NfcNci \
+    SecureElement \
     Tag
 
 PRODUCT_PACKAGES += \
-    nfc_nci.nqx.default.hw \
-    vendor.nxp.hardware.nfc@1.2-service
-
-PRODUCT_PACKAGES += \
-    nqnfcee_access.xml \
-    nqnfcse_access.xml
+    android.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
