12a3fc665ab6724ea6d2f74b7393acd0f7c82992
Keerten <keertenk@gmail.com>
2020-06-04 22:06:30 +0200

    miatoll: commonize dt2w overlays
    Change-Id: I625a7ed45cfe98a65d84b97975d0aa86c18e256c
    Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>

--- device.mk
+++ device.mk
@@ -191,7 +191,8 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-system \
+    $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
