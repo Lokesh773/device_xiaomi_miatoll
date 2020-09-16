6c63f6307516c649ab24e177240b5bcb7439b93b
Lokesh773 <lokeshkasamneni773@gmail.com>
2020-09-16 16:45:42 +0000

    miatoll:Fix derp during decommonising the tree
    *Move Inherit from products device.mk to lineage_miatoll.mk
    
    Signed-off-by: Lokesh773 <lokeshkasamneni773@gmail.com>

--- device.mk
+++ device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/miatoll/miatoll-vendor.mk)
 
--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
